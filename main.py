992490a5e9722745f1684208903f867515bb0ecf
ssmzhn <ssmzhn@163.com>
2022-07-28 20:32:36 +0800

    增加了地区码和出生日期的校验

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import datetime
-def isValidIdNumber(id_num):
+import json
+def somedayIsExists(year, month, day): # 判断某天是否存在
+    # 判断是否闰年
+    if year % 400 == 0:
+        isLeap = True
+    elif (year % 4 == 0) and (year % 100 != 0):
+        isLeap =  True
+    else:
+        isLeap = False
+    if month > 12 or month == 0:
+        return False
+    if day > 31 or day == 0:
+        return False
+    if day > 30 and month in (4,6,9,11):
+        return False
+    if month == 2:
+        if day > 28 and isLeap == False:
+            return False
+        if day > 29 and isLeap == True:
+            return False
+    return True
+def isValidIdNumber(id_num,gov_code):
     num_list=('0','1','2','3','4','5','6','7','8','9')
     num_X_list=('0','1','2','3','4','5','6','7','8','9','X','x')
     if len(id_num)!=18:
@@ -18,11 +39,19 @@ def isValidIdNumber(id_num):
         weight = 2**i%11
         a = int(id_num_list[i])
         verify += weight * a
+    # %11 != 1，证明非法
+    if verify % 11 != 1:
+        return False
 
-    # %11 == 1，证明合法
-    if verify % 11 == 1:
-        return True
-    return False
+    ##### 地区码校验 #####
+    if not id_num[:6] in gov_code:
+        return False
+
+    ##### 出生日期校验 #####
+    if not somedayIsExists(int(id_num[6:10]),int(id_num[10:12]),int(id_num[12:14])):
+        return False
+    return True
+
 def verifyDate(id_num): # 成没成年啊 id_num 为 str
     # birthday = datetime.date(int(id_num[6:10]),int(id_num[10:12]),int(id_num[12:14]))
     # 此处不用datetime.timedelta，因为不好处理年份
@@ -43,8 +72,10 @@ def verifyDate(id_num): # 成没成年啊 id_num 为 str
     return False
 
 def main():
+    f=open('govCode.json',encoding='UTF-8')
+    jsdict = json.load(f)
     test=input('让我开开盒罢: ')
-    if isValidIdNumber(test):
+    if isValidIdNumber(test,jsdict):
         print('是正常的身份证号捏')
         if verifyDate(test):
             print('您成年了！')
